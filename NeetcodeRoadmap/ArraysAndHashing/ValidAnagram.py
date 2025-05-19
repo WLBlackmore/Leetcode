b75a0ea0ca3d602c781ca679cd4be7ea921a034c
WLBlackmore <william.blackmore@mail.mcgill.ca>
2025-05-19 19:45:03 -0400

    Valid Anagram Complete

---
+++ NeetcodeRoadmap/ArraysAndHashing/ValidAnagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if (len(s) != len(t)):
+            return False
+        
+        occS = {}
+        occT = {}
+        
+        for i in range(len(s)):
+            s_char = s[i]
+            t_char = t[i]
+            
+            occS[s_char] = occS.get(s_char, 0) + 1
+            occT[t_char] = occT.get(t_char, 0) + 1
+            
+        return occS == occT
+
+# Time: O(n)
+# Space: O(n)
